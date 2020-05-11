7a1d398172ec6047d167b2dad5b219c90a9527fa
F89705C <andrea.romano2@external.marelli.com>
2020-05-11 16:50:54 +0200

    Added print for git diff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
+	std::cout << "test git diff" << std::endl;
 	std::cout << "Git Test" << std::endl;
 	std::cout << "Git commit 2" << std::endl;
 	return 0;
