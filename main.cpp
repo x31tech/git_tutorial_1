dcb5f8ec5f22fae96b5c57b9c8b2d08338440b3a
F89705C <andrea.romano2@external.marelli.com>
2020-05-11 16:58:28 +0200

    Test for modified line

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[])
 {
 	std::cout << "test git diff" << std::endl;
-	std::cout << "Git Test" << std::endl;
+	std::cout << "Git Test branch" << std::endl;
 	std::cout << "Git commit 2" << std::endl;
 	return 0;
 }
