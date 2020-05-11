622ad5f4e3f242c1653c1424aa5dec2aea857e1d
F89705C <andrea.romano2@external.marelli.com>
2020-05-11 17:05:31 +0200

    Added new line for a new commit on master for testing purpose

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "test git diff" << std::endl;
+	std::cout << "Test git diff" << std::endl;
 	std::cout << "Git Test" << std::endl;
 	std::cout << "Git commit 2" << std::endl;
+	std::cout << "Added new commit on master" << std::endl;
 	return 0;
 }
