fe90f6ac0ace6b4b6c1b47cfa2a46a4ddab2ff9c
F89705C <andrea.romano2@external.marelli.com>
2020-05-12 16:09:32 +0200

    Added new line for git fetch test

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
 	std::cout << "Added new commit on master" << std::endl;
 	std::cout << "Modification on feature_1 for test" << std::endl;
 	std::cout << "Added new commit on master" << std::endl;
+	std::cout << "Added new line for git fetch test" << std::endl;
 
 	function_1();
 	function_2();
