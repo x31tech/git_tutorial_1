05aacd3c9022014281de8e5f54eb217068ba586c
F89705C <andrea.romano2@external.marelli.com>
2020-05-12 10:57:05 +0200

    Added new line for feature

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
 	std::cout << "test git diff" << std::endl;
 	std::cout << "Git Test branch" << std::endl;
 	std::cout << "Git commit 2" << std::endl;
+	std::cout << "Modification on feature_1 for test" << std::endl;
 	return 0;
 }
